de91d939734725daece372ca1a5980353fa1cbb8
priscilay <priscila_franca1993@hotmail.com>
2019-12-10 14:46:44 -0300

    curso

--- CursoCucumber/src/test/java/AprenderCucumber.java
+++ CursoCucumber/src/test/java/AprenderCucumber.java
@@ -1,6 +1,12 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 import org.junit.Assert;
 
 import cucumber.api.java.pt.Dado;
+import cucumber.api.java.pt.Entao;
 import cucumber.api.java.pt.Então;
 import cucumber.api.java.pt.Quando;
 
@@ -8,37 +14,68 @@ public class AprenderCucumber {
 
 	@Dado("^que criei o arquivo corretamente$")
 	public void queCrieiOArquivoCorretamente() throws Exception {
-	  System.out.println("qualquer coisa");
+		System.out.println("qualquer coisa");
 	}
 
 	@Quando("^executa-lo$")
 	public void executaLo() throws Exception {
-	    
+
 	}
 
 	@Então("^a especificacao deve finalizar com sucesso$")
 	public void aEspecificacaoDeveFinalizarComSucesso() throws Exception {
-	    
+
 	}
-	
+
 	private int contador = 0;
-	
+
 	@Dado("^que o valor do contador é (\\d+)$")
 	public void queOValorDoContadorÉ(int arg1) throws Exception {
-	   contador = arg1;
+		contador = arg1;
 	}
 
 	@Quando("^eu incrementar em (\\d+)$")
 	public void euIncrementarEm(int arg1) throws Exception {
-	   contador = contador + arg1;
+		contador = contador + arg1;
 	}
 
 	@Então("^o contador sera (\\d+)$")
 	public void oContadorSera(int arg1) throws Exception {
-//	    System.out.println(arg1);
-//	    System.out.println(contador);
-//	    System.out.println(arg1 == contador);
-	    //Assert.assertTrue(arg1 == contador);
-	    Assert.assertEquals(arg1, contador);
+		// System.out.println(arg1);
+		// System.out.println(contador);
+		// System.out.println(arg1 == contador);
+		// Assert.assertTrue(arg1 == contador);
+		Assert.assertEquals(arg1, contador);
+	}
+
+	Date entrega = new Date();
+
+	@Dado("^que a entrega é dia (\\d+)/(\\d+)/(\\d+)$")
+	public void queAEntregaÉDia(int dia, int mes, int ano) throws Throwable {
+		Calendar cal = Calendar.getInstance();
+		cal.set(Calendar.DAY_OF_MONTH, dia);
+		cal.set(Calendar.MONTH, mes - 1);
+		cal.set(Calendar.YEAR, ano);
+		entrega = cal.getTime();
+	}
+
+	@Quando("^a entrega atrasar em (\\d+) (.+)$")
+	public void aEntregaAtrasarEmDias(int arg1, String tempo) throws Throwable {
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(entrega);
+		if(tempo.equals("dias")){
+			cal.add(Calendar.DAY_OF_MONTH, arg1);
+		}
+		if(tempo.equals("meses")){
+			cal.add(Calendar.MONTH, arg1);
+		}
+		entrega = cal.getTime();
+	}
+
+	@Entao("^a entrega sera efetuada em (\\d{2}\\/\\d{2}\\/\\d{4})$")
+	public void aEntregaSeraEfetuadaEm(String data) throws Throwable {
+		DateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+		String dataFormatada = format.format(entrega);
+		Assert.assertEquals(data, dataFormatada);
 	}
 }
--- CursoCucumber/src/test/java/aprender_cucumber.feature
+++ CursoCucumber/src/test/java/aprender_cucumber.feature
@@ -19,3 +19,12 @@ Cenario: Deve incrementar contador
 	Dado que o valor do contador é 20
 	Quando eu incrementar em 32
 	Então o contador sera 52
+	
+Cenario: Deve calcular atraso na entrega da china
+	Dado que a entrega é dia 05/04/2018
+	Quando a entrega atrasar em 2 meses
+	Entao a entrega sera efetuada em 05/06/2018
+	
+	
+	
+		
